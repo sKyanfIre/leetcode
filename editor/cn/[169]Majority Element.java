886c793782309ffe2e289b0589f093d77ee368e4
zhangtianyu <zhangtianyu@yupei-scm.com>
2020-12-04 15:10:54 +0800

    169.majority element 1.sort

---
+++ editor/cn/[169]Majority Element.java
@@ -0,0 +1,35 @@
+//Given an array of size n, find the majority element. The majority element is t
+//he element that appears more than ⌊ n/2 ⌋ times. 
+//
+// You may assume that the array is non-empty and the majority element always ex
+//ist in the array. 
+//
+// Example 1: 
+//
+// 
+//Input: [3,2,3]
+//Output: 3 
+//
+// Example 2: 
+//
+// 
+//Input: [2,2,1,1,1,2,2]
+//Output: 2
+// 
+// Related Topics 位运算 数组 分治算法 
+// 👍 811 👎 0
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int majorityElement(int[] nums) {
+        // 1.排序法
+//        Arrays.sort(nums);
+//        return nums[nums.length/2];
+        // 2.遍历法
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
