a24c031a2b6013edc30e56275d834df59fd35c43
zhangtianyu <zhangtianyu@yupei-scm.com>
2020-12-08 15:29:49 +0800

    21.merge two sorted lists 1.reserve

---
+++ editor/cn/[21]Merge Two Sorted Lists.java
@@ -0,0 +1,67 @@
+//Merge two sorted linked lists and return it as a new sorted list. The new list
+// should be made by splicing together the nodes of the first two lists. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: l1 = [1,2,4], l2 = [1,3,4]
+//Output: [1,1,2,3,4,4]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: l1 = [], l2 = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: l1 = [], l2 = [0]
+//Output: [0]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in both lists is in the range [0, 50]. 
+// -100 <= Node.val <= 100 
+// Both l1 and l2 are sorted in non-decreasing order. 
+// 
+// Related Topics 链表 
+// 👍 1425 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+//        if (l1 == null)
+//            return l2;
+//        if (l2 == null)
+//            return l1;
+//        if (l1.val > l2.val) {
+//            l2.next = mergeTwoLists(l1, l2.next);
+//            return l2;
+//        }
+//        l1.next = mergeTwoLists(l1.next, l2);
+//        return l1;
+
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
