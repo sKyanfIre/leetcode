9716da3c8b1b47e5b8e3d0f8b42f2bf222d4f396
zhangtianyu <zhangtianyu@yupei-scm.com>
2020-12-11 19:14:46 +0800

    142.linked list cycle II 1.快慢指针

---
+++ editor/cn/[142]Linked List Cycle II.java
@@ -0,0 +1,89 @@
+//Given a linked list, return the node where the cycle begins. If there is no cy
+//cle, return null. 
+//
+// There is a cycle in a linked list if there is some node in the list that can 
+//be reached again by continuously following the next pointer. Internally, pos is 
+//used to denote the index of the node that tail's next pointer is connected to. N
+//ote that pos is not passed as a parameter. 
+//
+// Notice that you should not modify the linked list. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [3,2,0,-4], pos = 1
+//Output: tail connects to node index 1
+//Explanation: There is a cycle in the linked list, where tail connects to the s
+//econd node.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2], pos = 0
+//Output: tail connects to node index 0
+//Explanation: There is a cycle in the linked list, where tail connects to the f
+//irst node.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = [1], pos = -1
+//Output: no cycle
+//Explanation: There is no cycle in the linked list.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of the nodes in the list is in the range [0, 104]. 
+// -105 <= Node.val <= 105 
+// pos is -1 or a valid index in the linked-list. 
+// 
+//
+// 
+// Follow up: Can you solve it using O(1) (i.e. constant) memory? 
+// Related Topics 链表 双指针 
+// 👍 790 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode quick = head;
+        ListNode slow = head;
+        boolean first = true;
+        while (quick != null && quick.next != null) {
+            if (quick == slow && !first) {
+                ListNode prev = head;
+                while (prev != slow) {
+                    prev = prev.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+            quick = quick.next.next;
+            slow = slow.next;
+            first = false;
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
